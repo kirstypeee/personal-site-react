b02ce8aa54bbdb0fd92828f67519a64dda7d32e6
Kirsty Purcell <kirstympurcell@gmail.com>
2023-07-11 14:23:23 +1000

    Wrap children in Fragment

--- src/routes/protectedRoute.tsx
+++ src/routes/protectedRoute.tsx
@@ -2,12 +2,12 @@ import { Navigate } from "react-router-dom";
 import { isEmpty } from "lodash";
 import { useAppSelector } from "../hooks/storeHooks";
 
-export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+export const ProtectedRoute = ({ children }: { children: React.ReactNode }): React.ReactNode => {
   const user = useAppSelector((state) => state.user.user);
 
   if (isEmpty(user.name) || isEmpty(user.company)) {
     return <Navigate to="/" replace />;
   }
 
-  return children;
+  return <>{children}</>;
 };
